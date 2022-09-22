5ea5e36ce72f47aab485f44d8e52229c3bffb764
derin <derinbino@gmail.com>
2022-09-21 23:58:16 -0500

    setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import firbase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -9,7 +9,12 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-
+  apiKey: "AIzaSyDDL1iLEzxv3AzstL-h2OSTDeFFVAiQm-w",
+  authDomain: "derins-chat-app.firebaseapp.com",
+  projectId: "derins-chat-app",
+  storageBucket: "derins-chat-app.appspot.com",
+  messagingSenderId: "460331765606",
+  appId: "1:460331765606:web:fe871474d220ca0b3242a7"
 })
 
 const auth = firebase.auth();
